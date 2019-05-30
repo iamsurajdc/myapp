2cbf0f46dad2e67086feeab22a20f30cb3ccbbb7
Suraj Chandgude <surajchandgude0304@gmail.com>
2019-05-30 12:46:09 +0530

    New Commit on 30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Suraj 30 ';
+  title = 'Angular Suraj Chandgude';
 }
